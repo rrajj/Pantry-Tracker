37b9e4fb426f07afd3d54d09b64691c37d0ba100
rrajj <rraj150997@gmail.com>
2024-08-01 12:41:13 -0700

    editted config

--- firebase.js
+++ firebase.js
@@ -4,23 +4,7 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getFirestore } from "@firebase/firestore";
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: process.env.API_KEY,
-//   authDomain: process.env.AUTH_DOMAIN,
-//   projectId: process.env.PROJECT_ID,
-//   storageBucket: process.env.STORAGE_BUCKET,
-//   messagingSenderId: process.env.MESSAGING_SENDER_ID,
-//   appId: process.env.APP_ID
-// };
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBB0Ry9wJoPsBHToKzqXgl0fZ3aiyctCiE",
-  authDomain: "hs-pantry-tracker-1236d.firebaseapp.com",
-  projectId: "hs-pantry-tracker-1236d",
-  storageBucket: "hs-pantry-tracker-1236d.appspot.com",
-  messagingSenderId: "774564384174",
-  appId: "1:774564384174:web:201ee94d6bf23e383ccc3f"
-};
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const fireStore = getFirestore(app);
