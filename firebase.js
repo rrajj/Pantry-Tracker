2c164475bd43e7d4f857253bbdf90b2f1bebcc0e
rrajj <rraj150997@gmail.com>
2024-08-05 10:16:48 -0700

    package

--- firebase.js
+++ firebase.js
@@ -8,12 +8,12 @@ import { getFirestore } from "@firebase/firestore";
 
 
 const firebaseConfig={
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBKufztuBKMUw6hzAWbam6lIB2lrTFZVko", //process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain:"hs-pantry-tracker-1fa0b.firebaseapp.com", // process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: "hs-pantry-tracker-1fa0b", //process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: "hs-pantry-tracker-1fa0b.appspot.com", //process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: "899839377000",//process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: "1:899839377000:web:913a181d42aa535e98c5dd"//process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 }
 
 // Initialize Firebase
